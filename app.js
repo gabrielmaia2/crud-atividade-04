5d20bab28414caee63c919be1a8bbaa098461234
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-11-17 19:17:12 -0300

    Create AlunoRoutes

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+const AlunoRoutes = require('./routes/AlunoRoutes');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/api/v1/alunos', AlunoRoutes);
 
 module.exports = app;
---
+++ routes/AlunoRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const AlunoService = require('../services/AlunoService');
+
+const AlunoRoutes = express.Router();
+
+module.exports = AlunoRoutes;
