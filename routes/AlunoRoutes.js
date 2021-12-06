2c3a0d3bedabe8126ed21172f69a58fbbbb4627b
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-12-06 09:47:14 -0300

    feat(aluno): Create add aluno route

--- routes/AlunoRoutes.js
+++ routes/AlunoRoutes.js
@@ -3,4 +3,15 @@ const AlunoService = require('../services/AlunoService');
 
 const AlunoRoutes = express.Router();
 
+AlunoRoutes.post('/post', (req, res) => {
+  const { nome, curso, IRA } = req.body;
+  const data = { nome, curso, IRA };
+
+  AlunoService.add(
+    data,
+    (_id) => res.json({ success: true, _id }),
+    () => res.json({ success: false })
+  );
+});
+
 module.exports = AlunoRoutes;
--- services/AlunoService.js
+++ services/AlunoService.js
@@ -1,6 +1,13 @@
 const AlunoModel = require('../models/AlunoModel');
 
 class AlunoService {
+  static add(data, res, err = () => {}) {
+    const { nome, curso, IRA } = data;
+    const aluno = { nome, curso, IRA };
+    AlunoModel.create(aluno)
+      .then((a) => res(a._id))
+      .catch(err);
+  }
 
 }
 
